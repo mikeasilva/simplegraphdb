a4bd06a4d28bfa8b67730f6b13f72cea52306e85
mikeasilva <mike.a.silva@gmail.com>
2021-02-17 09:21:48 -0500

    Added connect_nodes to NAMESPACE

--- R/connect_nodes.R
+++ R/connect_nodes.R
@@ -4,6 +4,7 @@
 #' @param target_id Target node's id
 #' @param properties Edge properties (optional)
 #' @import rjson
+#' @export
 connect_nodes <- function(source_id, target_id, properties = list()) {
   rjson::toJSON(properties)
   paste0("INSERT INTO edges VALUES(\"", as.character(source_id), "\", \"", as.character(target_id), "\", json('", rjson::toJSON(properties), "'));")
